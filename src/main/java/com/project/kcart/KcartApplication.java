b1a903788c3b3cacf07837d0e8c4114b4f616340
keshav <k21keshav05@gmail.com>
2025-10-11 00:22:10 +0530

    Inital Commit

---
+++ src/main/java/com/project/kcart/KcartApplication.java
@@ -0,0 +1,13 @@
+package com.project.kcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcartApplication.class, args);
+	}
+
+}
