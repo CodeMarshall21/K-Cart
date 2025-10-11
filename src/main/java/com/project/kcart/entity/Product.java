442c85af3bb80b50e1723ba98f11137f1523373e
keshav <k21keshav05@gmail.com>
2025-10-11 22:04:34 +0530

    Generate Getters and Setters

--- src/main/java/com/project/kcart/entity/Product.java
+++ src/main/java/com/project/kcart/entity/Product.java
@@ -1,5 +1,76 @@
 package com.project.kcart.entity;
 
 public class Product {
-    
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private Double rating = 0.0;
+    private String seller;
+    private Integer stock;
+    private Integer numOfReviews;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getNumOfReviews() {
+        return numOfReviews;
+    }
+
+    public void setNumOfReviews(Integer numOfReviews) {
+        this.numOfReviews = numOfReviews;
+    }
 }
