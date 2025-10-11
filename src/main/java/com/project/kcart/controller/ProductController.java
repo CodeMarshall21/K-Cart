b8a0507dbe2acb392c941fdd4ad782617fc931c0
keshav <k21keshav05@gmail.com>
2025-10-12 00:49:59 +0530

    Creating ProductController

--- src/main/java/com/project/kcart/controller/ProductController.java
+++ src/main/java/com/project/kcart/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.project.kcart.entity.Product;
 import com.project.kcart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
     public ResponseEntity<List<Product>> getAllProducts(){
         List<Product> products = productService.getAllProducts();
         return ResponseEntity.ok(products);
