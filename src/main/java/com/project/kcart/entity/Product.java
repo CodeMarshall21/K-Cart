333c492a6efae09e934faecf849d3d992a02fda9
keshav <k21keshav05@gmail.com>
2025-10-11 22:11:06 +0530

    Adding Entity annotations

--- src/main/java/com/project/kcart/entity/Product.java
+++ src/main/java/com/project/kcart/entity/Product.java
@@ -1,13 +1,40 @@
 package com.project.kcart.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Name field is required")
     private String name;
+
+    @Column(nullable = false)
+    @NotNull(message = "Price field is required")
+    @PositiveOrZero(message = "Value must be Zero or greater than zero")
     private Double price;
+
+    @NotBlank(message = "Description field is required")
     private String description;
+
+
     private Double rating = 0.0;
+
+    @NotBlank(message = "Seller field is required")
     private String seller;
+
+    @NotNull(message = "Stock field is required")
     private Integer stock;
+
+    
     private Integer numOfReviews;
 
     public Long getId() {
