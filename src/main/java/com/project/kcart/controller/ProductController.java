aafd302c4711479c90606b2fa7ce531dba78ee88
keshav <k21keshav05@gmail.com>
2025-10-14 23:45:12 +0530

    Adding Pagination for Product List API

--- src/main/java/com/project/kcart/controller/ProductController.java
+++ src/main/java/com/project/kcart/controller/ProductController.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Map;
 import java.util.List;
 
 
@@ -19,9 +20,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts(){
-        List<Product> products = productService.getAllProducts();
-        return ResponseEntity.ok(products);
+    public Map<String, Object> getAllProducts(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size){
+        return productService.getAllProducts(page, size);
     }
 
 }
