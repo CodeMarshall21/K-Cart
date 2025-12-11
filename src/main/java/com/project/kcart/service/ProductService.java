e0deb2a3736cae91fa6307cf01c1baa72d7ee2aa
keshav <k21keshav05@gmail.com>
2025-12-12 01:02:34 +0530

    Search Product API - Specification - hasCategory()

--- src/main/java/com/project/kcart/service/ProductService.java
+++ src/main/java/com/project/kcart/service/ProductService.java
@@ -2,10 +2,12 @@ package com.project.kcart.service;
 
 import com.project.kcart.entity.Product;
 import com.project.kcart.repository.ProductRepository;
+import com.project.kcart.spec.ProductSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -32,5 +34,11 @@ public class ProductService {
 
     public Product getProductById(Long id){
         return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public List<Product> searchProduct(String category, double minPrice, double maxPrice, String keyword, Double rating){
+        Specification<Product> spec = Specification.where(ProductSpecification.hasCategory(category));
+
+        return productRepository.findAll(spec);
     }
 }
---
+++ src/main/java/com/project/kcart/spec/ProductSpecification.java
@@ -0,0 +1,10 @@
+package com.project.kcart.spec;
+
+import com.project.kcart.entity.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductSpecification {
+    public static Specification<Product> hasCategory(String category){
+        return (root, query, criteriaBuilder) -> category == null ? null : criteriaBuilder.equal(root.get("category"),category);
+    }
+}
