f85a2ff77715ba3deea06762f56bc257f418743b
keshav <k21keshav05@gmail.com>
2025-12-15 02:01:41 +0530

    ProductReview.java - Adding relationship with Product Entity

--- src/main/java/com/project/kcart/entity/ProductReview.java
+++ src/main/java/com/project/kcart/entity/ProductReview.java
@@ -1,9 +1,6 @@
 package com.project.kcart.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
@@ -51,5 +48,17 @@ public class ProductReview {
 
     public ProductReview(){
         super();
+    }
+
+    @ManyToOne()
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
     }
 }
