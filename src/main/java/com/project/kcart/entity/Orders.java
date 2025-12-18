ebec44fe2a19acb4f52fb61c1cf1aebe5515b0dc
keshav <k21keshav05@gmail.com>
2025-12-19 02:30:04 +0530

    Creating Orders & OrderItem entities

---
+++ src/main/java/com/project/kcart/entity/Orders.java
@@ -0,0 +1,82 @@
+package com.project.kcart.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+    private Double totalItemsAmount;
+    private Double taxAmount;
+    private Double totalAmount;
+    private String status;
+    private String orderNo;
+
+    
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public Double getTotalItemsAmount() {
+        return totalItemsAmount;
+    }
+
+    public void setTotalItemsAmount(Double totalItemsAmount) {
+        this.totalItemsAmount = totalItemsAmount;
+    }
+
+    public Double getTaxAmount() {
+        return taxAmount;
+    }
+
+    public void setTaxAmount(Double taxAmount) {
+        this.taxAmount = taxAmount;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+}
